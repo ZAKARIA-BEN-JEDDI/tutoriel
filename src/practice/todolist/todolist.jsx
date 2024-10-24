5855a4d9ff77c00245dd777c9d2f3953c2a86c2f
ZakariaBj <zakariabenjeddi22@gmail.com>
2024-10-24 13:01:35 +0100

    Style non applique !

--- src/practice/todolist/todolist.jsx
+++ src/practice/todolist/todolist.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useRef, useEffect } from 'react';
-import './Todolist.css';
 export default function Todolist() {
   const [tache, setTache] = useState(() => {
     const savedTasks = localStorage.getItem('tache');
